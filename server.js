dc13d311f685fc258a321a56cf7ff8ae97eea47f
Shashank-000 <103059774+Shashank-000@users.noreply.github.com>
2022-07-12 20:38:57 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ var server = app.listen(8000,"127.0.0.1", function (){
     var port = server.address().port    
     console.log("server running at",port)
 });
-
+app.use(bodyParser.json() );
+app.use(bodyParser.urlencoded({
+}));
 var connection = mysql.createConnection({
     host :'localhost',
     user : 'root',
@@ -21,7 +23,7 @@ var connection = mysql.createConnection({
     console.log('Now connected to database!')
   })
 
-  //rest api to create a new record into mysql database
+  //api to create a new record into mysql database
     app.post('/insertpost', function (req, res) {
         var postData  = req.body;
         
@@ -32,7 +34,7 @@ var connection = mysql.createConnection({
     });
 
 
-  //rest api to See All  Records  of table
+  //api to See All  Records  of table
   app.get('/allpost',function(req,res){
     console.log(req);
     connection.query('SELECT * from blogs', function(error,results,fields){
@@ -43,7 +45,7 @@ var connection = mysql.createConnection({
   });
 
 
-   //rest api to get a single post data
+   // api to get a single post data
 app.get('/singlePost/:id', function (req, res) {
     connection.query('select * from blogs where id=?', [req.params.id], function (error, results, fields) {
        if (error) throw error;
@@ -51,10 +53,10 @@ app.get('/singlePost/:id', function (req, res) {
      });
  });
 
-  //rest api to delete record from  database
+  // api to delete record from  database
 app.delete('/delpost', function (req, res) {
     console.log(req.body);
-    connection.query('DELETE FROM `blogs` WHERE `id`=1',  function (error, results, fields) {
+    connection.query('DELETE FROM `blogs` WHERE `id`=?',[req.body.id],  function (error, results, fields) {
        if (error) throw error;
        res.end('Record has been deleted!');
      });
